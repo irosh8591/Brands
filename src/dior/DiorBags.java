3b430414e137f5a9920d1d09a51d1e9eeb339011
Iroda <imanakhunova91@mail.ru>
2024-01-29 17:31:45 +0600

    Revert "Dior bag new updated"
    This reverts commit 1c3835e6a963b34180254e54bdcbfc6def595f51.

--- src/dior/DiorBags.java
+++ src/dior/DiorBags.java
@@ -1,10 +1,4 @@
 package dior;
 
 public class DiorBags {
-    public static void main(String[] args) {
-        System.out.println("Classic bags");
-    }
-    public static void buyBags(){
-        System.out.println("The bag is beautiful");
-    }
 }
