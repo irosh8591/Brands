1c3835e6a963b34180254e54bdcbfc6def595f51
Iroda <imanakhunova91@mail.ru>
2024-01-29 17:30:46 +0600

    Dior bag new

--- src/dior/DiorBags.java
+++ src/dior/DiorBags.java
@@ -1,4 +1,10 @@
 package dior;
 
 public class DiorBags {
+    public static void main(String[] args) {
+        System.out.println("Classic bags");
+    }
+    public static void buyBags(){
+        System.out.println("The bag is beautiful");
+    }
 }
